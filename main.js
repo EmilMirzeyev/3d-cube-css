c5b512867748df634f75b9abbc28101724c34186
EmilMirzeyev <85557943+EmilMirzeyev@users.noreply.github.com>
2021-06-08 17:01:41 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+(function cubed (){
+    let rotateX = 0,
+        rotateY = 0;
+
+    document.onkeydown = function(e){
+        if (e.keyCode === 37) rotateY -=5
+    else if (e.keyCode === 38) rotateX +=5
+    else if (e.keyCode === 39) rotateY +=5
+    else if (e.keyCode === 40) rotateX -=5
+    document.querySelector('.cube').style.transform =
+    `rotateY(${rotateY}deg) rotateX(${rotateX}deg)`;
+    }
+})();
